7f183e0265cf3fd4b12102ad7928461b2814d812
usama-jamil <usamajamil006@gmail.com>
2019-04-23 19:58:07 +0500

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('',(req,res)=>{
+    res.send('<h1> Hello Express </h1>')
     res.send('Hello Express!')
 })
 
@@ -12,11 +13,14 @@ app.get('/help',(req,res)=>{
 })
 
 app.get('/about',(req,res)=>{
-    res.send('about Express!')
+    res.send('<h1> About </h1>')
 })
 
 app.get('/weather',(req,res)=>{
-    res.send('weather!')
+    res.send({
+        city:'karachi',
+        temperature:30
+    })
 })
 
 app.listen(3000 , ()=> console.log('Server is running on 3000'))
