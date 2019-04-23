631bbff64fa6b64c70356ff99c75ffe4dfd71bf6
usama-jamil <usamajamil006@gmail.com>
2019-04-23 22:53:30 +0500

    6.Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,8 +6,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryName = path.join(__dirname,'../public')
 
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryName))
 
+app.get('',(req,res)=>{
+    res.render('index',{
+        name:'usama Jmail'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'About me',
+        name: 'Usama Jamil'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title: 'dynamic  Handlebar file',
+        message: 'Usama Jamil'
+    })
+})
 
 app.get('/weather',(req,res)=>{
     res.send({
