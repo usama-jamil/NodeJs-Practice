fa6476f25c77d651ccee64f6e8b1e32748df07c0
usama-jamil <usamajamil006@gmail.com>
2019-05-03 17:48:25 +0500

    api for  task and users

--- restapi-app/src/db/mongoose.js
+++ restapi-app/src/db/mongoose.js
@@ -1,62 +1,11 @@
 const  mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser:true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User',{
-    name:{
-        type: String,
-        required: true,
-        trim: true
-    },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new  Error('Email is invalid')
-            }
-        }
-    },
-    age:{
-        type: Number,
-        default:0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a posotive Number')
-            }
-        }
-    },
-    password:{
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('You  cannot password as password')
-            }
-        }
 
-    }
-})
-
-const Task = mongoose.model('Task',{
-    description:{
-        type: String,
-        required:true,
-        trim: true 
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    }
-})
 
 // const me =new User({
 //     name: 'usama jamil',
@@ -66,14 +15,14 @@ const Task = mongoose.model('Task',{
 // me.save().then( res => console.log(res)).catch(err =>  console.log(err))
 
 
-const me =new User({
-    name: 'haris jamil',
-    age: '55',
-    email: 'usamajamil006@gmai.com',
-    password: 'passwordsdfds'
-})
+// const me =new User({
+//     name: 'haris jamil',
+//     age: '55',
+//     email: 'usamajamil006@gmai.com',
+//     password: 'passwordsdfds'
+// })
 
-me.save().then( res => console.log(res)).catch(err =>  console.log(err))
+// me.save().then( res => console.log(res)).catch(err =>  console.log(err))
 
 // const task = new Task({
 //     description:'flsjdflfksdjff flklkjje4iotjdknerk',
