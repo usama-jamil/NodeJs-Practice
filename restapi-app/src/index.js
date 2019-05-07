8cb67d6917b977efc9a42ee10bcdceabc4e52e23
usama-jamil <usamajamil006@gmail.com>
2019-05-07 17:49:24 +0500

    Update through  api

--- restapi-app/src/index.js
+++ restapi-app/src/index.js
@@ -66,6 +66,26 @@ app.get('/users/:id', async (req,res)=>{
     // })
 })
 
+app.patch('/users/:id', async (req , res) =>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name','email','password','age']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid Updates'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{ new:true , runValidators:true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
 
 app.post('/tasks',async(req,res)=>{
     const task = new Task(req.body)
@@ -116,6 +136,27 @@ app.get('/tasks/:id',async(req,res)=>{
     // }).catch(err =>{
     //     res.status(500).send(err)
     // })
+})
+
+app.patch('/tasks/:id', async (req,res) =>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['completed','description']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid Updates'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body,{ new:true , runValidators:true})
+
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, ()=>{
