8f9b216a8975ed1cba234f14f8060f0d52bd161f
usama-jamil <usamajamil006@gmail.com>
2019-05-10 16:17:06 +0500

    Logging in user

--- restapi-app/src/models/user.js
+++ restapi-app/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     },
     email:{
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -42,13 +43,30 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email,password) =>{
+    const user = await User.findOne({email})
+
+    if(!user){
+        throw new Error('Unable to Login')
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password)
+
+    if(!isMatch){
+        throw new Error('Unable to Login')
+    }
+
+    return user
+}
+
+// Change  Password to hash
 userSchema.pre('save', async function (next){
     const user = this
 
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password,8)
     }
-    
+
     next()
 })
 const User = mongoose.model('User',userSchema)
